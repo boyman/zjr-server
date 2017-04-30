62a88154055407a32019374c09e03268c6dd0fef
bobibi <bo.li.zeng@gmail.com>
2017-04-30 01:51:07 +0000

    Working version

--- routes/event.route.js
+++ routes/event.route.js
@@ -4,40 +4,56 @@ const LoginService = require('qcloud-weapp-server-sdk').LoginService;
 const Event = require('../models/event.model');
 
 const get_event = (req, res, next) => {
-	const loginService = LoginService.create(req, res);
-	loginService.check()
+    const loginService = LoginService.create(req, res);
+    loginService.check()
                 .then(data => {
-			Event.findById(req.query.id).exec()
-				.then(event => res.json({
-					code : 0,
-					message : 'ok',
-					event : event
-				}))
-				.catch(e => next(e));
-		});
+            Event.findById(req.query.id).exec()
+                .then(event => res.json({
+                    code : 0,
+                    message : 'ok',
+                    event : event
+                }))
+                .catch(e => next(e));
+        });
+};
+
+const get_my_host_events = (req, res, next) => {
+    const loginService = LoginService.create(req, res);
+    loginService.check().then(
+        data => {
+            Event.find({createdBy : data.userInfo.openId}).select('name dateTime').exec().then(
+                events => res.json({
+                    code : 0,
+                    message : 'ok',
+                    events : events
+                })
+            ).catch(e => next(e));
+        }
+    );
 };
 
 const add_event = (req, res, next) => {
-	const loginService = LoginService.create(req, res);
+    const loginService = LoginService.create(req, res);
 
-	loginService.check()
-		.then(data => {
-			const event = new Event(req.query);
-			event.createdBy = data.userInfo.openId;
-			console.log(req.query.date+' '+req.query.time);
-			event.dateTime = new Date(req.query.date+' '+req.query.time);
-			event.save()
-				.then(savedEvent => res.json({
-					code : 0,
-					message : 'ok',
-					savedEvent : savedEvent
-				}))
-				.catch(e => next(e));
+    loginService.check()
+        .then(data => {
+            const event = new Event(req.query);
+            event.createdBy = data.userInfo.openId;
+            console.log(req.query.date+' '+req.query.time);
+            event.dateTime = new Date(req.query.date+' '+req.query.time);
+            event.save()
+                .then(savedEvent => res.json({
+                    code : 0,
+                    message : 'ok',
+                    savedEvent : savedEvent
+                }))
+                .catch(e => next(e));
 
-		});
+        });
 };
 
 module.exports = {
-	get_event : get_event,
-	add_event : add_event
+    get_event : get_event,
+    get_my_host_events : get_my_host_events,
+    add_event : add_event
 }
--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/user', require('./user'));
 router.all('/tunnel', require('./tunnel'));
 router.get('/add_event', event.add_event);
 router.get('/get_event', event.get_event);
+router.get('/get_my_host_events', event.get_my_host_events);
 
 module.exports = router;
